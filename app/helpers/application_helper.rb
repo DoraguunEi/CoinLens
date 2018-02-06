747cbf2f344db198f6339bc97894aebcbed17026
zf-jiang <jack_jiangg@hotmail.com>
2018-02-05 18:39:15 -0800

    added full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	#full_title helper: returns base title if specific title is undefined
+	def full_title(page_title = ' ')
+		base_title = "CoinLens"
+		if page_title.empty?
+			base_title
+		else
+			page_title + " | " + base_title
+		end
+	end
 end
