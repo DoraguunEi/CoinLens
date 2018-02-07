90a7a3d496dff14ad473b023d68fc37deeee735e
zf-jiang <jack_jiangg@hotmail.com>
2018-02-06 19:21:11 -0800

    added register function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
-  def new
+  def show
+  	@user = User.find(params[:id])
+  	#debugger
   end
+
+  def register
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		flash[:success] = "Thank you for joining CoinLens."
+  		redirect_to @user
+  		# successful
+  	else
+  		render 'register'
+  	end
+  end
+
+  #initializing entire parameters hash is unsafe
+  #use strong parameters instead
+  private
+  	def user_params
+  		params.require(:user).permit(:name, :email, 
+  			:password, :password_confirmation)
+  	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,16 @@
 Rails.application.routes.draw do
-  root 'pages#home'
   #root 'welcome#index'
+  root 'pages#home'
 
+  #static pages
   get '/about', to: 'pages#about'
-
   get '/features', to: 'pages#features'
-
   get '/faq', to: 'pages#faq'
-
   get '/contact', to: 'pages#contact'
-
   get '/login', to: 'pages#login'
-
   get '/news', to: 'pages#news'
 
+  #users
   get '/register', to: 'users#register'
+  resources :users
 end
