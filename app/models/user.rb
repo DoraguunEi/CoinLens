f7621fac756d9a69b5e5646283ff0c541a6b3b64
zf-jiang <jack_jiangg@hotmail.com>
2018-02-11 21:40:45 -0800

    fix syntax error

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ApplicationRecord
 	#return hash digest of given string
 	def User.digest(string)
 		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-													  BCrypto::Engine.cost
+													  BCrypt::Engine.cost
 		BCrypt::Password.create(string, cost: cost)
 	end
 
