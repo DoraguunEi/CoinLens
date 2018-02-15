391b10344ecce64bc139e96ca5b7f12b22d98d45
Rai-2018 <w.liang1009@gmail.com>
2018-02-14 21:30:01 -0800

    list html and cointype model update1

--- test/controllers/list_controller_test.rb
+++ test/controllers/list_controller_test.rb
@@ -4,4 +4,17 @@ class ListControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  def showlist
+  end
+
+  def newCoin
+  end
+
+  def rmCoin
+  end
+
+  def editCoin
+  end
+
+  
 end
